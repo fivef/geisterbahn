51528ab1d8b782e4ee4be522eadddc6e7b31b9b6
fivef <fivef2@gmail.com>
2014-07-26 19:27:33 +0000

    Pir sensor and dmx fog working

--- bewegungsmelder_rauch.py
+++ bewegungsmelder_rauch.py
@@ -1,98 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import RPi.GPIO as GPIO
 import time
-import array
-from ola.ClientWrapper import ClientWrapper
 
-
-Counter = 0
+from dmx import DMX
 
 # debounce dead time in ms
 debounce_time = 5000
 
-pir_input_pin = 24
+pir_input_pin = 18
 
-smoke_output_pin = 23
-
-smoke_on_time_sec = 3
-
-#init dmx
-universe = 1
+dmx = None
 
 
-# pin reference: GPIO uses SoC GPIO numbers not the actual pin numbers (in diagram the number after GPIO, same for both pi revisions)
-GPIO.setmode(GPIO.BCM)  
+def initGPIOS():
 
-# set pin GPIO 24 as input, enable pull down resistor
-GPIO.setup(pir_input_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)  
+	# pin reference: GPIO uses SoC GPIO numbers not the actual pin numbers (in diagram the number after GPIO, same for both pi revisions)
+	GPIO.setmode(GPIO.BCM)  
 
-GPIO.setup(smoke_output_pin, GPIO.OUT)
+	# set pin GPIO 24 as input, enable pull down resistor
+	GPIO.setup(pir_input_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)  
+
+	#disable annoying warings
+	GPIO.setwarnings(False)
 
 # ISR
-def Interrupt(channel):  
+def movement_detected_callback(channel):  
 	
 	print "Movement detected" 
-	
-	GPIO.output(smoke_output_pin,1)
-	time.sleep(smoke_on_time_sec)
-	GPIO.output(smoke_output_pin,0)
-	#lightening()
-	#smoke()
+
+	dmx.lightening()
+	#dmx.smoke()
 	
 
-# add interrupt on pin pir_input_pin
-GPIO.add_event_detect(pir_input_pin, GPIO.RISING, callback = Interrupt, bouncetime = debounce_time)   
+  	print "Waiting for movement..."
 
 
-#dmx
-def DmxSent(state):
-	wrapper.Stop()
-	print "Dmx command successful"
-
-
-def lightening():
-
-	wrapper = ClientWrapper()
-	client = wrapper.Client()
-
-	"""B is bytecode"""
-	data = array.array('B')
-	data.append(255)
-	data.append(255)
-	data.append(255)
-	data.append(255)
-	data.append(255)
-
-	client.SendDmx(universe, data)
-	wrapper.Run()
+def main():
 	
-	time.sleep(2)
+	initGPIOS()
+
+	# add interrupt on pin pir_input_pin
+	GPIO.add_event_detect(pir_input_pin, GPIO.RISING, callback = movement_detected_callback, bouncetime = debounce_time) 
+
+	global dmx
+
+	dmx = DMX()
+
+	print "Waiting for movement..."
+
+	try:
+    
+    		while True:
+        		time.sleep(1000)
+	except KeyboardInterrupt:
+    		GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+	GPIO.cleanup()           # clean up GPIO on normal exit
 	
-	"""B is bytecode"""
-	data = array.array('B')
-	data.append(0)
-	data.append(0)
-	data.append(0)
-	data.append(0)
-	data.append(0)
 
-	client.SendDmx(universe, data)
-	wrapper.Run()
-	
-	wrapper.Terminate()
-	
-	
-def smoke():
-
-	wrapper = ClientWrapper()
-	client = wrapper.Client()
-
-	"""B is bytecode"""
-	data = array.array('B')
-	data.append(255)
-
-	client.SendDmx(universe, data)
-	wrapper.Run()
-
-
-while True:
-	time.sleep(1)
+if __name__ == "__main__":
+    main()
---
+++ dmx.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ola.ClientWrapper import ClientWrapper
+import array
+import time
+#dmx
+
+class DMX:
+
+	universe = 1
+
+	smoke_strength = 255
+	smoke_on_time_s = 3
+
+	def __init__(self):
+
+		#print "Make sure the lightbar is in 5 channel mode and on channel 1"
+		print "Make sure the fog machine is on channel 1
+		self.initDmx()
+		
+
+	def initDmx(self):
+		self.wrapper = ClientWrapper()
+		self.client = self.wrapper.Client()
+
+
+	def DmxSent(self, state):
+
+		if state.Succeeded():
+			print "Sent successfully"
+
+
+	def lightening(self):
+
+		"""B is bytecode"""
+		data = array.array('B')
+		
+		#lightbar in 5 cannel mode!!!
+
+		data.append(255) #RED
+		data.append(255) #GREEN
+		data.append(255) #BLUE
+		data.append(255) #BRIGHTNESS
+		data.append(255) #STROBO
+
+		self.client.SendDmx(self.universe, data, self.DmxSent)
+	
+		time.sleep(4)
+
+		data = array.array('B')
+		data.append(0)
+		data.append(0)
+		data.append(0)
+		data.append(0)
+		data.append(0)
+
+		self.client.SendDmx(self.universe, data, self.DmxSent)
+
+	
+	
+	def smoke(self):
+
+		data = array.array('B')
+		data.append(self.smoke_strength)
+
+		self.client.SendDmx(self.universe, data, self.DmxSent)
+
+		time.sleep(self.smoke_on_time_s)
+
+		data = array.array('B')
+		data.append(0)
+
+		self.client.SendDmx(self.universe, data, self.DmxSent)
+
+		
+
